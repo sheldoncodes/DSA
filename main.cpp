cc774bc87ae9500f7b7eaee754674e89d76dc0ef
shivam arora <85953131+sheldon-o@users.noreply.github.com>
2022-01-12 19:37:01 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,22 @@
+
 #include <iostream>
+using namespace std;
 
 int main() {
   std::cout << "Hello World!\n";
-  
+  int a;
+  a=12;
+  cout<<"size of int"<<sizeof(a)<<endl;
+  float b;
+  cout<<"size of float"<<sizeof(b)<<endl;
+  char c;
+  cout<<"size of char"<<sizeof(c)<<endl;
+  bool n;
+  cout<<"size of bool"<<sizeof(n)<<endl;
+
+  short int si;
+  long int li;
+  cout<<"size of long int"<<sizeof(li)<<endl;
+  cout<<"size of short int"<<sizeof(si)<<endl;
+
 } 
