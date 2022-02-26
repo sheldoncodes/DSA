e4dbbef473224e14ad96e49195e3d567799eb666
shivamArora <shivam4dec20@gmail.com>
2022-02-26 20:44:56 +0530

    gg

---
+++ strings/formbiggestnumber/formbiggestnum.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<string>
+#include<algorithm>
+using namespace std;
+int main(){
+    string num="37468";
+    sort(num.begin(),num.end(),greater<int>());
+    cout<<num<<num;
+    string s="aeieqpmsancjnawfeeijfvnxmnoo";
+    
+
+
+
+}
