5a300535e03546d625817c399048b65ba1348199
shivam arora <85953131+sheldon-o@users.noreply.github.com>
2022-03-11 19:16:32 +0530

    Create revision.cpp
    sometimes u feel low ,ahh

---
+++ revision.cpp
@@ -0,0 +1,128 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+#include<climits>
+using namespace std;
+
+int main() {
+    // Write C++ code here
+    int a,b,c,sum;
+    char ch;
+    sum=0;
+   // cin>>ch;
+    
+    
+    cin>>a;
+   // cin>>b;
+//    cin>>c;
+
+
+
+
+// prime no
+/*
+int i,j;
+for( i=2;i<=a;i++){
+    for( j=2;j<i;j++){
+        if(i%j==0){
+            break;
+        }
+        }
+    if(i==j){
+        cout<<i<<endl;
+    }
+}
+*/
+
+ //primeno
+ /*
+ cout<<"what";
+    int i;
+    for(i=2;i<a;i++){
+     if(a%i==0){
+         cout<<"not a prime"<<endl;
+         break;
+     }
+ }
+ if(i==a){
+     cout<<"primeno"<<endl;
+     
+ }
+ */
+ 
+
+  /* // odd numbers
+  for(int i=1;i<=a;i++){
+       if(i%2==0){
+           continue;
+       }cout<<i<<endl;
+   }
+   */
+
+/* // multiplication table
+    for(int i=1;i<=10;i++){
+        cout<<a*i<<"\n";
+    }
+*/
+
+/*// sum of n natural numbers
+    for(int i=1;i<=a;i++){
+        sum+=i;
+    }
+    cout<<sum;
+    
+    */
+
+
+
+   /* //vowel or consonat
+     int islowercase,isuppercase;
+    islowercase=(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u');
+    isuppercase=(ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U');
+    if(islowercase||isuppercase){
+        cout<<"vowel";
+    }else{
+        cout<<"not a vowel";
+    }
+    */
+    
+    
+    
+   /*{ if(a>b){
+        if(a>c){
+            cout<<"a is greatest\n";
+        }else{
+            cout<<"c is the greatest\n";
+        }
+    }else{
+        if (b>c){
+            cout<<"b is the greatest\n";
+        }else{
+            cout<<"c is the greatest\n";
+        }
+    }*/
+    
+    
+    
+    
+  /*  max between two { if (a==b){
+        cout<<"equal\n";
+    }
+    else if(a>b){
+        cout<<"a is greater\n";
+    }else{
+        cout<<"b is greater\n";
+    }
+   }*/
+    
+    
+   /* //odd even {
+    if(a%2==0){
+        cout<<"even";
+    }
+    else{
+        cout<<"odd";
+    }
+    }*/
+    
+    return 0;
+}
