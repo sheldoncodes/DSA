8f468bc445d1a1416df3a8c15a91ae3ae469d41e
shivam arora <85953131+sheldon-o@users.noreply.github.com>
2022-03-24 00:04:35 +0530

    Update revision.cpp

--- revision.cpp
+++ revision.cpp
@@ -396,4 +396,72 @@ for(int i=1;i<=n;i++){
         }
         cout<<endl;
     }*/
+       //pyramid pattern for numbers
+    
+   /* for(int i=1;i<=n;i++){
+        //spaces
+        for(int j=1;j<=n-i;j++){
+            cout<<"#";
+        }
+        for(int j=1;j<=i;j++){
+            cout<<i<<" ";
+        }cout<<endl;
+    }
+    // pyramid pattern 2
+        for(int i=1;i<=n;i++){
+        //spaces
+        for(int j=1;j<=n-i;j++){
+            cout<<"#";
+        }
+        for(int j=1;j<=i;j++){
+            cout<<j<<" ";
+        }cout<<endl;
+    } */
+    
+    
+    //solid butterfly
+  /*  for(int i=1;i<=n;i++){
+        for(int j=1;j<=2*n;j++){
+            if (j<=i || j>2*n-i){
+                cout<<"*";
+            }else{
+                cout<<"#";
+        }
+    }cout<<endl;
+    
+    }
+    for(int i=n;i>=1;i--){
+        for(int j=2*n;j>=1;j--){
+            if (j<=i || j>2*n-i){
+                cout<<"*";
+            }else{
+                cout<<"#";
+            }
+        }cout<<endl;
+    }
+    */
+    
+    
+    
+    //hollow butterfly
+  /*     for(int i=1;i<=n;i++){
+        for(int j=1;j<=2*n;j++){
+            if (j==1 || j== i || j ==2*n || j==2*n-i+1){
+                cout<<"*";
+            }else{
+                cout<<"#";
+        }
+    }cout<<endl;
+    
+    }
+    for(int i=n;i>=1;i--){
+        for(int j=2*n;j>=1;j--){
+            if (j==1 || j== 2*n || j ==i || j== 2*n-i+1){
+                cout<<"*";
+            }else{
+                cout<<"#";
+            }
+        }cout<<endl;
+    }
+    */
 
