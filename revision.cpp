5aa8295fc6803ad0497e3e44f8a312ddfd8aa170
shivam arora <85953131+sheldon-o@users.noreply.github.com>
2022-03-27 17:45:12 +0530

    Update revision.cpp

--- revision.cpp
+++ revision.cpp
@@ -464,4 +464,106 @@ for(int i=1;i<=n;i++){
         }cout<<endl;
     }
     */
+    // Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin>>n;
+    
+/* pascal triangle
+
+    int coef=1;
+    for(int i=0;i<n;i++){
+        for(int j=n;j>n-i;j--){
+            cout<<" ";
+        }
+        for(int k=0;k<=i;k++){
+            if(k==0 || i==0){
+                coef=1;
+            }else{
+                coef=coef*(i-k+1)/k;
+            }
+            cout<<coef<<" ";
+        }
+        cout<<endl;
+        
+    }
+    
+    */
+    
+    /* ZIG ZAG PAtTERN
+    
+    for(int i=1;i<=3;i++){
+        for(int j=1;j<=n;j++){
+            if(((i+j)%4==0 )|| ((i==2) && (j%4==0))){
+                cout<<"*";
+                
+            }else{
+                cout<<"#";
+            }
+        }cout<<endl;
+    }
+    
+    */
+    /*print digit right to left
+    while(n>0){
+        int rem=n%10;
+        cout<<rem;
+        n=n/10;
+    }
+    */
+    /* ARMSTRONG NUMBER
+    int total=0;
+    int temp=n;
+    while(n>0){
+        int rem=n%10;
+        total=total+(rem*rem*rem);
+        n=n/10;
+    }
+    if(total==temp){
+        cout<<"chacha vidhayak hai humare";
+    }else{
+        cout<<"boooooa";
+    }
+    */
+    /*factorial;
+    int fac=1;
+    for (int i=1;i<=n;i++){
+        fac=fac*i;
+    }
+    cout<<fac;
+    */
+/* fibbonaici upto n
+        int a=0;
+        int b=1;
+    for(int i=1;i<=n;i++){
+        
+        
+        cout<<a<<" ";
+        int temp=b;
+         b=a+b;
+         a=temp;
+        
+        
+    }
+*/
+
+//reverse a number
+    /*
+int rev=0;
+int rem;
+while(n>0){
+    rem=n%10;
+    n=n/10;==
+    rev=rev*10+rem;
+    
+    
+}
+cout<<rev;
+*/
+    return 0;
+}
+
 
